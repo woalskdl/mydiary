4960a2b9dde8b9baf364a942e687ecfff51b52af
jay <woalskdl@gmail.com>
2024-01-14 23:11:45 +0900

    firebase / 로그인 연동 및 database 저장 / recoil 저장 (여전히 ios build 안되며, android 로그인 안됨 >> 해당 이슈 해결 필요)

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { RecoilRoot } from 'recoil';
 import { RootApp } from "./src/RootApp";
+
+GoogleSignin.configure();
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <RootApp/>
+      <RecoilRoot>
+        <RootApp/>
+      </RecoilRoot>
     </SafeAreaProvider>
   );
 }
--- src/SplashView.js
+++ src/SplashView.js
@@ -1,20 +1,78 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { View } from "react-native";
+import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database';
+import { useSetRecoilState } from 'recoil';
 import { Typography } from "./components/Typography";
+import { stateUserInfo } from "./states/stateUserInfo";
 
 export const SplashView = (props) => {
+    const [showLoginButton, setShowLoginButton] = useState(false);
+    const setUserInfo = useSetRecoilState(stateUserInfo);
+
+    const signinUserIdentify = useCallback(async (idToken) => {
+        console.log('idToken >> ', idToken);
+        const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+        const result = await auth().signInWithCredential(googleCredential);
+
+        console.log('result >> ', result);
+        const userDBRefKey = `/users/${result.user.uid}`;
+        const userResult = await database().ref(userDBRefKey).once('value').then((snapshot) => {
+            return snapshot.val();
+        })
+
+        console.log(userResult);
+        const now = new Date().toISOString();
+
+        if(userResult === null) {
+            await database().ref(userDBRefKey).set({
+                name:result.additionalUserInfo.profile.name,
+                profileImage:result.additionalUserInfo.profile.picture,
+                uid:result.user.uid,
+                password:'',
+                createdAt:now,
+                lastLoginAt:now
+            })
+        } else {
+            await database().ref(userDBRefKey).update({
+                lastLoginAt:now,
+            })
+        }
+
+        const userInfo = await database().ref(userDBRefKey).once('value').then((snapshot) => snapshot.val());
+
+        console.log('userInfo >> ', userInfo);
+
+        setUserInfo(userInfo);
+        props.onFinishLoad();
+
+    }, [])
+
+    const onPressGoogleLogin = useCallback(async () => {
+        await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog:true});
+        const { idToken } = await GoogleSignin.signIn();
+        signinUserIdentify(idToken);
+
+    }, [])
+
+    const userSilentLogin = useCallback(async () => {
+        try {
+            const { idToken } = await GoogleSignin.signInSilently();
+            signinUserIdentify(idToken);
+        } catch(ex) {
+            setShowLoginButton(true);
+        }
+    }, [])
 
     useEffect(() => {
-        setTimeout(() => {
-            props.onFinishLoad();
-        }, 2000)
-    }, [])
+        userSilentLogin();
+    }, []);
 
     return (
         <View style={{ flex:1, alignItems:'center', justifyContent:'center' }}>
-            <Typography fontSize={26}>
-                SPLASH
-            </Typography>
+            {showLoginButton && <GoogleSigninButton onPress={onPressGoogleLogin}/>}
         </View>
     )
 }
